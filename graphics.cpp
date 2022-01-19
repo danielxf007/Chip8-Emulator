afd518d4c13ecba50454bf8ca17807edec4833dd
danielxf007 <daniel.higuita007@gmail.com>
2022-01-19 10:17:40 -0500

    Clears the screen with fill rect

--- graphics.cpp
+++ graphics.cpp
@@ -12,21 +12,23 @@ Graphicator::Graphicator(u32 screen_w, u32 screen_h, GFXMem *gfx, const char *wi
     this->window = SDL_CreateWindow(window_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
      screen_w, screen_h, this->window_flags);
     SDL_SetSurfaceRLE(SDL_GetWindowSurface(window), 1);
+    window_surface = SDL_GetWindowSurface(window);
 }
 
 Graphicator::~Graphicator(){
     SDL_DestroyWindow(window);
+    window_surface = NULL;
+    SDL_Quit();
 }
 
 void Graphicator::renderScreen(){
-    SDL_Surface *surface = SDL_GetWindowSurface(window);
-    SDL_LockSurface(surface);
-    SDL_memset(surface->pixels, 0xFFFFFFFF, screen_h*(surface->pitch));
-    u32 *screen = (u32*) surface->pixels;
+    SDL_LockSurface(window_surface);
+    SDL_FillRect(window_surface, NULL, 0x000000);
+    u32 *screen = (u32*) window_surface->pixels;
     for(u32 n_row = 0; n_row < screen_h; n_row++){
         for(u32 m_col = 0; m_col < screen_w; m_col++)
             screen[screen_w*n_row + m_col] = gfx->mem_ptr[(n_row/10)*64 + m_col/10]? 0xFFFFFFFF: 0;
     }
-    SDL_UnlockSurface(surface);
+    SDL_UnlockSurface(window_surface);
     SDL_UpdateWindowSurface(window);
 }
